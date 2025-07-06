390249d2cb6c7e9b0d52413d26f2c3617255d89e
asselikhov <selikhov58@yandex.ru>
2025-07-06 16:24:44 +0300

    Bot 2.1.4

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ console.log('All required environment variables are set');
 
 // Подключение MongoDB
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB: ' + process.env.MONGODB_URI.replace(/\/\/.*@/, '//[hidden]@')))
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log('Connected to MongoDB: ' + process.env.MONGODB_URI.replace(/\/\/.*@/, '//[hidden]@'));
+    })
     .catch(err => {
-      console.error('MongoDB connection error:', err);
+      console.error('MongoDB connection error: ' + err.message);
       process.exit(1);
     });
 
